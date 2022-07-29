0b10d97d1e7dc861da40dbc20adf8b79dd068116
Victor Han <liyanghan1112@gmail.com>
2022-07-29 09:17:57 -0500

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Victor Han on 2022/7/29.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
