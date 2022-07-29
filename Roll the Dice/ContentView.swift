87be1f5f8df862a64f4cda079a3066c78e8b5b36
Victor Han <liyanghan1112@gmail.com>
2022-07-29 09:28:11 -0500

    Created initial view

--- Roll the Dice/ContentView.swift
+++ Roll the Dice/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var randomValue = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Dice Roll")
+                .font(.title)
+                .padding()
+            Text("\(randomValue)")
+                .font(.system(size: 72))
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                }
+            Spacer()
+        }
     }
 }
 
